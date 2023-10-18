f50a05f4041a408499420ba6f3a6deb794c7abee
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-18 11:17:31 +0200

    test kwNEw

--- src/kreuzwort/kreuzwortController.js
+++ src/kreuzwort/kreuzwortController.js
@@ -1,28 +1,21 @@
 class KreuzwortController extends Function {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
-    async parseCall(body){
-        return {
-            "Test": "controller"
+    async parseCall(body) {
+        if (body.type == 1) {
+            const kwNew = require('./kreuzwortNew')
+            let res = await kwNew(body)
+            return res;
         }
-        // if (body.type == 1) {
-    //     const kwNew = require('./kreuzwortNew')
-    //     let res = await kwNew(body)
-    //     return res;
-    //     return {
-    //         Test: 'hat geklappt'
-    //     }
-    // }
-    // if (body.type == 0) {
-    //     const kwUpdate = require('./kreuzwortUpdate')
-    //     let res = await kwUpdate(body)
-    //     return res
-    // }
+        if (body.type == 0) {
+            // const kwUpdate = require('./kreuzwortUpdate')
+            // let res = await kwUpdate(body)
+            // return res
+        }
     }
-    
+
 }
 
-module.exports = new KreuzwortController;
-        
+export default new KreuzwortController;
--- src/kreuzwort/kreuzwortNew.js
+++ src/kreuzwort/kreuzwortNew.js
@@ -1,7 +1,10 @@
 export default async function(body){
-    const db = require("./kreuzwortDb")
-    const split = require("./kreuzwortNewSplitter")
-    let quiz = await db.getRandomQuiz(body.userCount)
-    let res = await split(quiz, body.users, body.room)
-    return res
+    // const db = require("./kreuzwortDb")
+    // const split = require("./kreuzwortNewSplitter")
+    // let quiz = await db.getRandomQuiz(body.userCount)
+    // let res = await split(quiz, body.users, body.room)
+    // return res
+    return {
+        "answer": body.type
+    }
 }

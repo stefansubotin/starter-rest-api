f807f774f56c812b7b7f4a2e38c36f98516322a9
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-23 17:01:33 +0200

    debug test

--- src/other/simpleQuestionDb.js
+++ src/other/simpleQuestionDb.js
@@ -4,20 +4,16 @@ class SimpleQuestionDb extends Function {
     async getFullList() {
         const items = await db.getFullList('simpleQuestion');
         console.log('Full List');
-        console.log(items);
+        //console.log(items);
         return items;
     }
 
     async getWwmList(){
         let full = await this.getFullList();
-        console.log('fullList');
-        console.log(full);
         let results = [];
         for (let i = 0; i < full.results.length; i++){
             let item = await this.getItem(full.results[i].key);
-            console.log(item);
             if (item.props.answerCount == 4) {
-                console.log('added item')
                 results.push(full.results[i]);
             }
         }

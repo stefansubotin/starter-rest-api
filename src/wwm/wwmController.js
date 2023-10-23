dac37aa651925c729699c1281dbdd854eeb66839
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-23 16:35:19 +0200

    debug

--- src/wwm/wwmController.js
+++ src/wwm/wwmController.js
@@ -6,8 +6,14 @@ class TabooController extends Function {
     async parseCall(body) {
         const creator = require('./wwmCreator');
         const messenger = require('./wwmMessenger');
+        console.log('start parsing call...');
+        console.log('creating quiz...');
         let quiz = await creator.createQuiz();
+        console.log('done creating quiz!');
+        console.log('start sending messages...');
         await messenger.sendMessages(quiz, body.moderator, body.player, body.room);
+        console.log('done sending messages!');
+        console.log('done parsing call!');
     }
 }
 
